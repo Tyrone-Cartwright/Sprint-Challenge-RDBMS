198eec403dbd9c386bbf7a2ec4b80473576b421c
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-18 12:53:23 -0500

    Added project/actions seeds for data

--- migrations/20190118114422_project.js
+++ migrations/20190118114422_project.js
@@ -1,8 +1,22 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("projects", function(tbl) {
+    // Primary Key
+    tbl.increments();
+
+    // Name
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique();
+
+    // Description
+    tbl.text("description", "longtext");
+
+    // Project complete
+    tbl.boolean("complete").defaultTo(false);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("projects");
 };
--- migrations/20190118114448_action.js
+++ migrations/20190118114448_action.js
@@ -1,8 +1,26 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", function(tbl) {
+    // Primary Key
+    tbl.increments();
+
+    // Description
+    tbl.text("description");
+
+    // Notes
+    tbl.text("notes");
+
+    // Action complete
+    tbl.boolean("complete").defaultTo(false);
+
+    // Relationship
+    tbl
+      .integer("project_id")
+      .unsigned()
+      .references("id")
+      .inTable("projects");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("actions");
 };
